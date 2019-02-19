59e0fd73b2cd082127d228d627a13d1da43e7cff
Rufino Rojas <lucras015@hotmail.com>
2019-02-19 16:30:58 -0600

    se agrego flatlist

--- App.js
+++ App.js
@@ -13,16 +13,19 @@ export default class App extends Component {
   placeAdd = placeName => {
     this.setState(prevState => {
       return{
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDelete = index =>{
+  placeDelete = key =>{
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
