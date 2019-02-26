08beaefdd4b2296f0964c21fa664111388bd54a6
Rufino Rojas <lucras015@hotmail.com>
2019-02-26 14:32:22 -0600

    se agrego react-navigation y los archivos de navecacion de ios y android

--- ios/demoReact/AppDelegate.h
+++ ios/demoReact/AppDelegate.h
@@ -1,14 +1,18 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 
-#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <ReactNativeNavigation/ReactNativeNavigation.h>
 
-@property (nonatomic, strong) UIWindow *window;
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
+  
+  return YES;
+}
 
 @end
